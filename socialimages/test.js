ee61c5c520dc75a624bbc6aa31c46923d7768e1d
ZThonBot <130180385+ZThonBot@users.noreply.github.com>
2023-04-07 23:37:42 +0300

    Add files via upload

---
+++ socialimages/test.js
@@ -0,0 +1,30 @@
+(async () => {
+	switch (command) {
+case "tt": {
+let Languagey = require("./lib/scraper");
+let Langt = Languagey.getString("global");
+
+          let buttonMessaged = {
+           image:{ url : picsecktor},
+            caption: `Did you mean ${prefix}help?`,
+            footer: ` ` + LangG.footer,
+            headerType: 4,
+            contextInfo: {
+              externalAdReply: {
+                title: LangG.title,
+                body: LangG.dotbody,
+                thumbnail: log0,
+                mediaType: 2,
+                mediaUrl: ``,
+                sourceUrl: ``,
+              },
+            },
+          };
+
+          await Void.sendMessage(citel.chat, buttonMessaged, {
+            quoted: citel,
+          });
+}
+        break
+		}
+})()
